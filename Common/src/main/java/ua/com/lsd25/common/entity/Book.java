630463c0a88ea589bef464332bbf5beb7952304e
Victor Zagnitko <victor.zagnitko@yandex.ru>
2014-03-31 21:32:12 +0300

    Added Book entity
    Signed-off-by: Victor Zagnitko <victor.zagnitko@yandex.ru>

---
+++ Common/src/main/java/ua/com/lsd25/common/entity/Book.java
@@ -0,0 +1,11 @@
+package ua.com.lsd25.common.entity;
+
+import com.google.code.morphia.annotations.Entity;
+
+/**
+ * @author Victor Zagnitko on 31.03.2014.
+ *         This entity describe Book
+ */
+@Entity(noClassnameStored = true)
+public class Book {
+}
---
+++ Common/src/main/java/ua/com/lsd25/common/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by Victor Zagnitko on 31.03.2014.
+ */
+package ua.com.lsd25.common;
